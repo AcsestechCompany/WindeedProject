c7585116fabc6c7acc3fc1be3f853fd527c56c4b
Farhana <shaikfarhanacse@gmail.com>
2018-05-24 12:17:18 +0530

    commit

--- src/app/RegisterPage.js
+++ src/app/RegisterPage.js
@@ -3,6 +3,8 @@ import {container} from "bootstrap";
 import styles from "./Layout.css";
 import {Register} from "./Register";
 import {Login} from "./Login";
+import {thinDown} from 'react-icons-kit/entypo/thinDown';
+import {Icon} from "react-icons-kit";
 
 
 export class RegisterPage extends React.Component{
@@ -31,7 +33,30 @@ export class RegisterPage extends React.Component{
       <div class={styles.background}>
       <p class={styles.header}>
       <span class={styles.logo}>Windeed</span>
-      <span class={styles.divider}></span>
+      <p id={styles.divider}></p>
+
+
+        <span class="dropdown" id={styles.jobseekerdropdown}>
+
+    <button id={styles.dropdown} type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  JOB SEEKER <Icon icon={thinDown} class={styles.arrowdown}/>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" id={styles.dropdownmenu}>
+      <a class="dropdown-item" href="#">Search Jobs</a>
+      <a class="dropdown-item" href="#">Search Resume</a>
+    </div>
+    </span>
+
+  <span class="dropdown" id={styles.employerdropdown}>
+  <button id={styles.dropdown} type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  EMPLOYER <Icon icon={thinDown} class={styles.arrowdown}/>
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" id={styles.dropdownmenu}>
+  <a class="dropdown-item" href="#">Search Talent</a>
+  <a class="dropdown-item" href="#">Post Jobs</a>
+
+  </div>
+  </span>
       <span class={styles.premium}>PREMIUM PLANS</span>
       <span class={styles.headerright}>
       <span onClick={this.handleLogin.bind(this)} class={styles.login}>LOG IN</span>
--- src/app/Test.js
+++ src/app/Test.js
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./Layout.css";
+
+
 
 export class Test extends React.Component{
+
   render(){
     return(
-      <div>In the Test Component </div>
+      <div>
+
+
+       </div>
     );
   }
 }
